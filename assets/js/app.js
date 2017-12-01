95fc80fc95972cfc6b89d9130684a4e045fc75be
Kist221 <Kist221@gmail.com>
2017-11-30 20:40:10 -0700

    ajax in place

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
 // NYT Search App
 var APIkey = "f944a4982edc4da3aa536bb309f9fcf4";
 
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+APIkey+"?q="+query;
 
+var query = "sample";
+
+// url += '?' + $.param({
+//   'api-key': "f944a4982edc4da3aa536bb309f9fcf4",
+//   'q': "query",
+//   'begin_date': "20000101",
+//   'end_date': "20170707"
+// });
+
+
+
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
